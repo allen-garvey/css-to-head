917fecfd813865f63cae3efcae3d1defaefa89fe
Allen Garvey <allengarvey@hotmail.com>
2015-09-18 23:41:02 -0400

    add check to see if style tag already exists, and append to it if it does

--- css-header.rb
+++ css-header.rb
@@ -3,10 +3,16 @@ require 'nokogiri'
 html_path = "test.html"
 page = Nokogiri::HTML(open(html_path))
 
+
 page.css('link[rel=stylesheet]').each do |link_element|
 	css_path = link_element['href']
 	css = File.read(css_path)
-	page.css('head')[0].add_child("<style>" +  css +  "</style>")
+	style_elements = page.css('style')
+	if style_elements.length > 0
+		style_elements[0].content = style_elements[0].content + css
+	else
+		page.css('head')[0].add_child("<style>" +  css +  "</style>")
+	end
 	link_element.remove
 end
 puts page
